c84cdcfc3aafeabccdb273cd348af88eb2e87ce0
opplaypro <93275481+opplaypro@users.noreply.github.com>
2024-10-24 11:50:52 +0200

    Update minesweeper.py

--- minesweeper/terminal/minesweeper.py
+++ minesweeper/terminal/minesweeper.py
@@ -6,26 +6,11 @@ import os
 import time
 
 
-#   import pygame
-#   from pygame import mouse
-
 os.system('cls')
-
-'''
-changes to make (find with ctr+f):
-format: change0001
-
-change0001: change frame rate to 60
-change0002: change input method
-
-for other things to fix look for comments with "TO_FIX"
-'''
-
-
 
 
 # function to choose difficulty level
-# change input method to button on UI instead of text input #change0002
+
 def choose_difficulty():
     while True:
         difficulty = input("Wybierz poziom trudności: ")
@@ -45,115 +30,69 @@ def choose_difficulty():
             continue
 
 
-
-
-'''
-# button place and size
-button_1_pos = ()
-button_1_size = ()'''
-
-
-
-
 def change_numbers(grid, x, y):
     if grid[x][y] != 9:
         grid[x][y] += 1
     return grid
 
 
-
-
 #   funcion sets every position to number of bombs near
 #   rewrite using less code if possible
 def numbers_near_bombs(grid, diff):
-
     #   IT WORKS !!! BEFORE CHANGING TEST IF IT WORKS THE SAME
     #   GRID = [[]]
-
-    horizontal = 9
-    vertical = 9
-
-    if diff == 0:
-        horizontal = 9
-        vertical = 9
-
-    elif diff == 1:
-        horizontal = 29
-        vertical = 19
-
-    elif diff == 2:
-        horizontal = 59
-        vertical = 44
-
+    horizontal, vertical = [9, 29, 59][ diff], [9, 19, 44][ diff]
     r = c = 0
 
     for row in grid:
-
         for box in row:
-
             if r == 0:
-
                 # only for first row
                 if c == 0:
-
                     # if in first column
                     if box == 9:
-
                         grid = change_numbers(grid, r, c + 1)
                         grid = change_numbers(grid, r + 1, c + 1)
                         grid = change_numbers(grid, r + 1, c)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c < horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r, c + 1)
                         grid = change_numbers(grid, r, c - 1)
                         grid = change_numbers(grid, r + 1, c - 1)
                         grid = change_numbers(grid, r + 1, c)
                         grid = change_numbers(grid, r + 1, c + 1)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c == horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r, c - 1)
                         grid = change_numbers(grid, r + 1, c - 1)
                         grid = change_numbers(grid, r + 1, c)
                         c = 0
                         r += 1
-
                     else:
                         c = 0
                         r += 1
-
                 else:
                     c = 0
                     r += 1
-
             elif r < vertical:
                 if c == 0:
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r - 1, c + 1)
                         grid = change_numbers(grid, r, c + 1)
                         grid = change_numbers(grid, r + 1, c)
                         grid = change_numbers(grid, r + 1, c + 1)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c < horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c - 1)
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r - 1, c + 1)
@@ -163,13 +102,10 @@ def numbers_near_bombs(grid, diff):
                         grid = change_numbers(grid, r + 1, c)
                         grid = change_numbers(grid, r + 1, c + 1)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c == horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c - 1)
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r, c - 1)
@@ -177,61 +113,46 @@ def numbers_near_bombs(grid, diff):
                         grid = change_numbers(grid, r + 1, c)
                         c = 0
                         r += 1
-
                     else:
                         c = 0
                         r += 1
-
                 else:
                     c = 0
                     r += 1
-
             elif r == vertical:
                 if c == 0:
                     # if in first column
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r - 1, c + 1)
                         grid = change_numbers(grid, r, c + 1)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c < horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c - 1)
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r - 1, c + 1)
                         grid = change_numbers(grid, r, c - 1)
                         grid = change_numbers(grid, r, c + 1)
                         c += 1
-
                     else:
                         c += 1
-
                 elif c == horizontal:
                     if box == 9:
-
                         grid = change_numbers(grid, r - 1, c - 1)
                         grid = change_numbers(grid, r - 1, c)
                         grid = change_numbers(grid, r, c - 1)
                         c = 0
                         r += 1
-
                     else:
                         c = 0
                         r += 1
-
                 else:
                     c = 0
                     r += 1
-
     return grid
-
-
 
 
 # sets grid size and places bombs depending on difficulty
@@ -244,14 +165,12 @@ def set_bombs(diff):
             x = random.randint(0, 9)
             y = random.randint(0, 9)
             bombs_coords[y][x] = 9
-
     elif diff == 1:
         bombs_coords = [[0 for _ in range(30)] for _ in range(20)]
         for i in range(120):
             x = random.randint(0, 29)
             y = random.randint(0, 19)
             bombs_coords[y][x] = 9
-
     elif diff == 2:
         bombs_coords = [[0 for _ in range(60)] for _ in range(45)]
         for i in range(300):
@@ -259,8 +178,6 @@ def set_bombs(diff):
             y = random.randint(0, 44)
             bombs_coords[y][x] = 9
     return bombs_coords
-
-
 
 
 # test if everything works !!! TERMINAL ONLY!!! NO PYGAME
@@ -286,8 +203,6 @@ def test_script():
     print("\n\n\n")
     for i in corrected:
         print(i)
-
-
 
 
 def print_grid(grid, diff):
@@ -322,10 +237,7 @@ def print_grid(grid, diff):
             k += 1
 
 
-
-
 def player_guess(grid, diff):
-
     play = True
     max_x, max_y = [9, 29, 59][diff], [9, 19, 44][diff]
     if diff not in [0, 1, 2]:
@@ -339,12 +251,15 @@ def player_guess(grid, diff):
         time.sleep(0.1 * random.randint(3, 7))
         print("Trwa Ładowanie", k+1, "/10")
     print("Jeżeli wybrałeś poziom trudności średni albo trudny, musisz zmienić rozmier okna i rozmier czcionki (ctrl + scroll)")
+
     for k in range(6):
         time.sleep(0.1 * random.randint(3, 7))
         print("Trwa Ładowanie", k+5, "/10")
+
     os.system('cls')
     visited_cells = []
     print_grid(visible_grid, diff)
+
     while play:
         guess = input("Wprowadź współrzędne: ").split(",")
         os.system('cls')
@@ -362,15 +277,11 @@ def player_guess(grid, diff):
             else:
                 if 0 <= x <= max_x and 0 <= y <= max_y:
                     visible_grid, play, visited_cells = uncover_cells(grid, visible_grid, diff, (int(x), int(y)), visited_cells)
-
                 else:
                     print("error, za dużo")
-
         else:
             print("error")
             continue
-
-
 
 
 def show_cells_near(grid, visible_grid, diff, guess, visited_cells):
@@ -398,8 +309,6 @@ def show_cells_near(grid, visible_grid, diff, guess, visited_cells):
     return visible_grid, visited_cells
 
 
-
-
 def uncover_cells(grid, visible_grid, diff, guess, visited_cells):
     x = guess[0]
     y = guess[1]
@@ -418,12 +327,8 @@ def uncover_cells(grid, visible_grid, diff, guess, visited_cells):
     return visible_grid, play, visited_cells
 
 
-
-
 def first_interaction():
     print("Witaj w grze Saper!\nJeśli pojawią się błędy, zgłoś proszę!")
-
-
 
 
 def game():
@@ -434,8 +339,6 @@ def game():
     player_guess(bomb_grid, diff)
 
 
-
-
 #   change to 1 if you want to use test function instead if game()
 TESTS = 0
 
@@ -444,40 +347,3 @@ if __name__ == "__main__":
         test_script()
     else:
         game()
-
-
-#   below is pygame part of the code
-#   (at least I want to add it)
-
-'''
-WIDTH = 800
-HEIGHT = 480
-
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Minesweeper")
-clock = pygame.time.Clock()
-running = True
-width = screen.get_width()
-height = screen.get_height()
-while running:
-
-    mouse = pygame.mouse.get_pos()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        print(event)
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            check_if_bomb(mouse[0],mouse[1])
-
-
-
-
-
-    clock.tick(10)  #change later to 60 (10 is more visible in terminal) find with change0001
-pygame.quit()
-'''
